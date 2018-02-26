ddc6e14b962fd1723710a22e5276db4a122ad8eb
Martinson Michael 599 <mmartinson@yandex.ru>
2018-02-26 04:01:05 +0300

    change conversation with postgre

--- Task_1/consumer/recieve.py
+++ Task_1/consumer/recieve.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import pika
 import time
+import psycopg2
 
 if __name__ == "__main__":
 
@@ -11,11 +12,19 @@ if __name__ == "__main__":
 
 	connection = pika.BlockingConnection(pika.ConnectionParameters(host='queue'))
 	channel = connection.channel()
-
 	channel.queue_declare(queue='hello_2')
 
+
+	db_conn = psycopg2.connect(dbname='postgres', user='postgres', host='db', password='q12q', port='5432')
+	cursor = db_conn.cursor()
+	cursor.execute("CREATE TABLE IF NOT EXISTS strings (str CHAR(255))")
+	db_conn.commit()
+
 	def callback(ch, method, properties, body):
-	    print(" [x] Received :'%r'" % body, flush=True)
+	    s = str(body, 'utf-8')
+	    print(" [x] Received %r" % s, flush=True)
+	    cursor.execute("INSERT INTO strings (str) VALUES (\'{}\')".format(s))
+	    db_conn.commit()
 	
 
 	channel.basic_consume(callback,
